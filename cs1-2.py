f6ebefec336d26c09bed6cfb082301b4a08d2e34
p22cobrien <p22cobrien@fairfieldprep.org>
2021-10-12 10:16:51 -0400

    update percent formula

--- cs1-2.py
+++ cs1-2.py
@@ -1,10 +1,10 @@
 # Author: CMOB 10/12/2021
 
 p = input("What is the initial investment amount? ")
-r = float(input("What is the annual interest rate as a percent? "))
+r = input("What is the annual interest rate as a percent? ")
 t = input("What is the amount of time in years that the money is invested? ")
 n = float(12)
-r /= 100
+r = float(r) / 100
 
 a = float(p) * (1 + (float(r) / n)) ** (n * float(t))
 
