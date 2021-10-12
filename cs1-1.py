1d055ab07caf112a8729ad21ad80576ec7572316
p22cobrien <p22cobrien@fairfieldprep.org>
2021-10-12 10:14:47 -0400

    add cs1-1.py

---
+++ cs1-1.py
@@ -0,0 +1,10 @@
+# Author: CMOB 10/12/2021
+
+x_1 = input("what is the 1st x coordinate? ")
+y_1 = input("what is the 1st y coordinate? ")
+x_2 = input("what is the 2nd x coordinate? ")
+y_2 = input("what is the 2nd y coordinate? ")
+
+distance = ((((float(x_2) - float(x_1)) ** 2) + ((float(y_2) - float(y_1)) ** 2)) ** 0.5)
+
+print(distance)
